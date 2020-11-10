e5cdda1de543cac35b76a4d43240b9d1e3c2c352
gili-Katagiri <kt.giri1425@gmail.com>
2020-11-10 15:40:39 +0900

    refactoring Stocker with tests

--- lib/stocker/stocker.py
+++ lib/stocker/stocker.py
@@ -26,6 +26,7 @@ class StockerFilePath(SytraPath):
         
         root_path.mkdir()
         (root_path/'log').mkdir()
+        (root_path/'log'/'trash').mkdir()
         (root_path/'prepare').mkdir()
         (root_path/'holidays').mkdir()
 
@@ -34,12 +35,11 @@ class StockerFilePath(SytraPath):
 
     # return concrete path
     # get directory and path
-    def get_stockpath( self, scode: int, target: str='stock.csv', noerror=False):
-        filepath = self._get_filepath(scode, target, noerror=noerror)
-        dirpath = filepath.parent
-        return dirpath, filepath
+    def get_stockpath( self, scode: int, noerror=False):
+        return self._get_filepath(scode, noerror=noerror)
     # get directory path
     def get_logpath(self): return self._get_filepath('log')
+    def get_trashpath(self): return self.get_logpath()/'trash'
     def get_preparepath(self): return self._get_filepath('prepare')
     def get_holidayspath(self): return self._get_filepath('holidays')
     # get file path
@@ -48,15 +48,6 @@ class StockerFilePath(SytraPath):
     def get_sbasepath(self, noerror=True):
         return self._get_filepath('summary_base.csv', noerror=noerror)
     
-    # candidate name to available name
-    def get_available_path(self, fname: Path):
-        candstem, candsuf = fname.stem, fname.suffix
-        candname = fname
-        while( fname.exists() ):
-            cnt += 1
-            candname = (fname.parent) / (candstem+str(cnt)+candsuf)
-        return candname
-
 
 # handling files
 class StockerFile(StockerFilePath):
@@ -67,50 +58,21 @@ class StockerFile(StockerFilePath):
 
     # file <=> dict
     def _load_conf( self )-> dict:
-        with super().get_confpath().open() as f:
-            tomldic = toml.load(f)
+        tomlstr = super().get_confpath().read_text()
+        tomldic = toml.loads(tomlstr)
         return tomldic
     def _dump_conf( self, tomldic: dict):
-        with super().get_confpath().open(mode='w') as f:
-            toml.dump( tomldic, f)
+        tomlstr = toml.dumps(tomldic)
+        super().get_confpath().write_text(tomlstr)
     
-    # prepare/*.csv -> */primitive.csv
-    def _follow_deploy(self)-> List[int]:
-        path = super().get_preparepath()
-        stocks = []
-        for f in path.glob('*.csv'):
-            # /path/to/stocks/prepare/9999.csv -> 9999
-            code = int( f.stem )
-            dname, fname = super().get_stockpath(code,target='primitive.csv',
-                    noerror=True)
-            try: dname.mkdir()
-            except FileExistsError:
-                print('WARNING: Passing {0}, directory already exists.'\
-                        .format(dname))
-                continue
-            f.rename(fname)
-            stocks.append(code)
-        return stocks
-    
-    def _defollow_process(self, stock_code: int, candname: str):
+    def _defollow_process(self, stock_code: int):
         # get path
-        dname, fname = super().get_stockpath(stock_code, target='stock.csv')
-        # newname is not always available
-        newname = dname / candname
-        newname = super().get_available_path(newname)
-        # rename stock.csv-> candname
-        fname.rename(newname)
-    
-    # get file's bottom line
-    @classmethod 
-    def _filecount( cls, filepath: Path)-> int:
-        return sum( 1 for _ in filepath.open() )
-    @classmethod
-    def _filetail( cls, filepath: Path)-> List[str]:
-        size = cls._filecount(filepath)
-        line = linecache.getline( str(filepath), size )
-        data = [ s.strip() for s in line.split(',') ]
-        return data
+        dname = super().get_stockpath(stock_code)
+        # 9999/ -> log/trash/9999
+        trashpath = super().get_trashpath()
+        newname = trashpath / str(stock_code)
+        # rename
+        dname.rename(newname)
     
     def _create_sbase(self, daystr: str, follows: Tuple[int])-> pd.DataFrame:
         RCOL = self.__class__.RECEPTION_COLUMNS
@@ -126,8 +88,7 @@ class StockerFile(StockerFilePath):
         # get path
         sbase_path = super().get_sbasepath()
         # write
-        with sbase_path.open(mode='w', encoding='cp932') as f:
-            f.write( daystr+'\n' )
+        sbase_path.write_text( daystr+'\n', encoding='cp932')
         sbase.to_csv( sbase_path, mode='a', index=False, encoding='cp932')
         return sbase
 
@@ -151,16 +112,14 @@ class StockerFile(StockerFilePath):
         by, fy = str(year-1), str(year+1)
         holidays += [by+'-12-31',fy+'-01-01',fy+'-01-02',fy+'-01-03']
         return holidays
+
     # files -> list ->  data
     def _set_sytra_holidays(self)-> List[str]:
         holiroot = super().get_holidayspath()
-        addlist = []
         for f in holiroot.glob('*.csv'):
             year = f.stem
             holist = self._get_holidays_list(f, int(year))
             SytraDay.add_holidays_list( year, holist)
-            addlist.append(year)
-        return addlist
 
 class Stocker(StockerFile):
 
@@ -174,14 +133,14 @@ class Stocker(StockerFile):
         # define sytra/env
         envpath = Path(__file__+'/../../../env').resolve()
         print('Make env file at %s' % str(envpath))
-        with envpath.open(mode='w') as f: f.write(str(rootpath))
+        envpath.write_text(str(rootpath))
 
         # call sytra_days_init and get year
         daysdic = SytraDay.sytra_days_init(daystr)
         tomldic = { 'stocker':{ 'follows': follows ,'days': daysdic } }
-        
-        with (rootpath/'sytraconf.toml').open(mode='w') as f:
-            toml.dump( tomldic, f)
+        tomlstr = toml.dumps(tomldic)
+
+        (rootpath/'sytraconf.toml').write_text(tomlstr)
         
 
     def __init__( self, rootdir: str):
@@ -189,71 +148,53 @@ class Stocker(StockerFile):
         super().__init__(rootdir)
         self._load()
 
-    # save and load
-    def _load(self):
-        tomldic = super()._load_conf()
-        addlist = super()._set_sytra_holidays()
-        self._set_bytoml(tomldic)
-    def dump(self):
-        tomldic = self._to_tomldic()
-        super()._dump_conf(tomldic)
-    
-    # toml <=> instance
-    def _set_bytoml( self, tomldic: dict):
-        # set follows list
-        self._follows_list = tomldic['stocker']['follows']
-
-        # protocol: add holidays before day init
-        daystr = tomldic['stocker']['days']['latest']
-        self._latest_update_day = SytraDay(daystr)
-    def _to_tomldic(self)-> dict:
-        return {'stocker':
-                    {
-                        'follows': self._follows_list,
-                        'days': self._latest_update_day.to_tomldic()
-                    }
-                }
-
-    # return str, Not as it is
+    # utility
     def get_follows_tuple(self)-> Tuple[int]:
         return tuple(self._follows_list)
     def get_lateststr(self)-> str:
         return self._latest_update_day.get_daystr()
     def get_nextdaystr(self)-> str:
         return self._latest_update_day.get_nextstr()
-
-    # check
     def _code_in_follows(self, stock_code: int)-> bool:
         return stock_code in self.get_follows_tuple()
-    # summary.csv must NOT exists when calls follow process
     def _check_follow_callable(self)-> bool:
         try: super().get_summarypath()
         except SytraPathError: return True
         return False
-
     # follow
-    def _follow_stock(self, stock_code: int, **kwds):
-        # check stock_code has not been followed
-        if self._code_in_follows(stock_code): raise StockerError('StockKeyError')
-        dname, fname = self.get_stockpath(stock_code, target='primitive.csv')
+    def _follow_stock(self, stock_code: int):
+        # create 9999/
+        dname = super().get_stockpath(stock_code)
         Analyzer.analyzer_init(dname, self.get_lateststr())
-        
         # add stock_code to _follows_list
         self._follows_list.append( stock_code )
 
     # defollow
-    def _defollow_stock(self, stock_code: int, **kwds):
-        # check stock_code has been followed
-        if not self._code_in_follows(stock_code): raise StockerError('StockKeyError')
-
-        # 9999/stock.csv go to 9999/renames
-        renames = kwds['renames']
-        renames = 'rm'+str(stock_code)+'.csv' if renames == '' else renames
-        super()._defollow_process(stock_code, renames)
-
-        # remove stock_code by _follows_list
+    def _defollow_stock(self, stock_code: int):
+        # 9999/ -> log/trash/9999/
+        self._defollow_process(stock_code)
+        # remove stock_code from _follows_list
         self._follows_list.remove(stock_code)
 
+    # prepare/*.csv -> */primitive.csv
+    def _follow_deploy(self)-> List[int]:
+        path = super().get_preparepath()
+        stocks = []
+        for f in path.glob('*.csv'):
+            # /path/to/stocks/prepare/9999.csv -> 9999
+            code = int( f.stem )
+            if self._code_in_follows(code):
+                print('WARNING: Ignore %d process, which is already followed.'\
+                        % (code))
+            else:
+                # mkdir 9999/ and mv 9999.csv 9999/primitive.csv
+                dname = super().get_stockpath(code, noerror=True)
+                dname.mkdir()
+                f.rename(dname/'primitive.csv')
+                stocks.append(code)
+        return stocks
+
+    '''
     # allocate
     def _allocate_stock(self, stock_code: int, **kwds):
         # get row data from kwds dictionary and to DataFrame
@@ -268,13 +209,13 @@ class Stocker(StockerFile):
 
         # write as csv
         rowdata.to_csv( fname, mode='a', header=False, index=False)
+    '''
 
-
-    def _iterate_process(self, proc, codes: List[int], options: dict):
+    def _iterate_process(self, proc, codes: List[int]):
         failure_list = []
         for code in codes:
             print('Processing %d...' % code, end='')
-            try: proc(code, **options)
+            try: proc(code)
             except StockerError as e:
                 failure_list.append(code)
                 print(e)
@@ -284,7 +225,7 @@ class Stocker(StockerFile):
 
 
     # interfaces
-    def follow_interface( self, codes: List[int]=[], defollow: bool=False, deploy: bool=True, force: bool=False, sort: bool=True, renames: str='', **elsekwds):
+    def follow_interface( self, codes: List[int]=[], defollow: bool=False, deploy: bool=True, force: bool=False, sort: bool=True, **elsekwds):
         """
         The reason for preparing dictionary:options is to process functions _follow_stock and _defollow_stock as same arguments.
         Options included _follow_stock:
@@ -299,50 +240,66 @@ class Stocker(StockerFile):
 
         As default, call _follow_deploy and _follow_stock at codes.
         """
-
+        
+        # proc: _follow_stock or _defollow_stock
         proc = self._defollow_stock if defollow else self._follow_stock
-        options = {'renames': renames}
-
+        # summary.csv have to be not exits
         if not ( force or self._check_follow_callable() ):
-            print('Already summary.csv has prepared!')
+            print('[Warning]: Already summary.csv has prepared!')
             print('If force process, please add -f/--force option.')
             return codes
+
+        # callable check
+        for code in codes:
+            # follow process need not exists the code in follows: False
+            # defollow process need to exists the code in follows: True
+            if self._code_in_follows is defollow:
+                print('[WARNING]: %d is irregal position.'%(code))
+                return codes
         
+        # deploy prepare/ with check callable
         if not defollow and deploy:
-            print('Deploy the prepare directory.')
-            codes += super()._follow_deploy()
+            print('Deploy the prepare directory ...')
+            codes += self._follow_deploy()
         
-        flist = self._iterate_process( proc, codes, options )
-        if flist: print('failuer: ', flist)
+        # processing
+        flist = self._iterate_process( proc, codes )
+        if flist: print('[WARNING]: Failuer codes: ', flist)
 
+        # post-process
         if sort: self._follows_list.sort()
-
         return flist
 
-    def allocate_interface(self, **kwds):
+    def allocate_interface(self):
+
         # load
         spath = super().get_summarypath()
         daystr, summary = super()._get_summary(spath)
         codes = list(summary.index)
+
         # check 
         if daystr!=self.get_nextdaystr(): raise StockerError('StockDayError')
         if tuple(codes)!=self.get_follows_tuple():
             raise StockerError('StockKeyError')
-
-        # dictionaly's key must be string!
-        # summary_dic = { "code1": pd.Series(Open, High, ...),
-        #                 "code2": pd.Series(Open, High, ...), ...}
-        summary.index = summary.index.astype(str)
-        summary_dic = summary.T.to_dict(orient='series')
-        self._iterate_process( self._allocate_stock, codes, summary_dic)
         
+        # get dmode 
+        cdate = SytraDay(daystr)
+        dmode = cdate.get_dmode()
+
+        # allocate
+        for code, rowx in summary.iterrows():
+            # create Analyzer interface
+            dpath = super().get_stockpath(code)
+            analy = Analyzer(dpath)
+            analy.daily_update(cdate, dmode, rowx)
+        
+        # post-process
         logpath = super().get_logpath() / (daystr.replace('-',''))
         spath.rename(logpath)
-
         self._latest_update_day.day_advance()
         
     # create sbase.csv
-    def create_sbase(self, **kwds):
+    def create_sbase(self):
         daystr = self.get_nextdaystr()
         follows = self.get_follows_tuple()
         
@@ -350,3 +307,27 @@ class Stocker(StockerFile):
         print(daystr)
         print(sbase.head(1).to_csv(None, index=False))
         print('...')
+
+    # save and load
+    def _load(self):
+        tomldic = super()._load_conf()
+        super()._set_sytra_holidays()
+        self._set_bytoml(tomldic)
+    def dump(self):
+        tomldic = self._to_tomldic()
+        super()._dump_conf(tomldic)
+    # toml <=> instance
+    def _set_bytoml( self, tomldic: dict):
+        # set follows list
+        self._follows_list = tomldic['stocker']['follows']
+        # protocol: add holidays before day init
+        daystr = tomldic['stocker']['days']['latest']
+        self._latest_update_day = SytraDay(daystr)
+    def _to_tomldic(self)-> dict:
+        return {'stocker':
+                    {
+                        'follows': self._follows_list,
+                        'days': self._latest_update_day.to_tomldic()
+                    }
+                }
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,39 @@
 import sys
 from pathlib import Path
 
-lib = Path(__file__, '../../lib').resolve()
+lib = Path(__file__,'../../lib').resolve()
 sys.path.insert(0, str(lib))
+
+from stocker import Stocker
+import shutil
+import pytest
+
+testroot = Path(__file__+'/../testocks/').resolve()
+presets = Path(__file__+'/../.presetdata').resolve()
+
+h2020 = (presets/'h2020').read_text()
+h2021 = (presets/'h2021').read_text()
+s4382 = (presets/'s4382').read_text()
+s6890 = (presets/'s6890').read_text()
+sum0713 = (presets/'sum0713').read_text()
+
+@pytest.fixture(scope='module')
+def sytra_file_fixture():
+    if testroot.exists():
+        shutil.rmtree(str(testroot))
+
+    # create directory and sytraconf.toml
+    Stocker.stocker_init(rootdir=str(testroot), daystr='2020-07-10')
+        
+    # directory for holidays
+    (testroot/'holidays'/'2020.csv').write_text(h2020)
+    (testroot/'holidays'/'2021.csv').write_text(h2021)
+
+    # directory for prepare
+    (testroot/'prepare'/'4382.csv').write_text(s4382)
+    (testroot/'prepare'/'6890.csv').write_text(s6890)
+    
+    # sbase.csv
+    (testroot/'summary.csv').write_text(sum0713)
+
+    yield str(testroot)
